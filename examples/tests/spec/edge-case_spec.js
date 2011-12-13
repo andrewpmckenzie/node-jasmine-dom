fd47e6ef9931de7b9dd3a072628c8e370bb75a71
Andrew McKenzie <andrew_mckenzie@bnz.co.nz>
2011-12-13 15:52:38 +1300

    Problem one: jQuery trigger doesn't work

--- examples/tests/spec/edge-case_spec.js
+++ examples/tests/spec/edge-case_spec.js
@@ -1,9 +1,15 @@
 describe('edge cases', function(){
 
-        it('fails', function(){
-            expect('foo').toEqual('bar');
-            console.log('console log');
-            console.log('console log2');
+        it('allows triggering of actions', function(){
+
+            var checkedRadioID = function(){
+                return $('#trigger_test input:checked').attr('id');
+            };
+            
+            expect( checkedRadioID() ).toEqual( 'radio_2' );
+            $('#radio_3').trigger('click');
+            expect( checkedRadioID() ).toEqual( 'radio_3' );
+
         });
 
 });
